d1aa6b2d4dccbef33b3693f2db31323918687302
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-16 20:40:09 +1100

    Validation email

--- app/src/main/java/com/collab/LogIn.java
+++ app/src/main/java/com/collab/LogIn.java
@@ -2,30 +2,48 @@ package com.collab;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LogIn extends AppCompatActivity implements View.OnKeyListener {
 
+    TextInputEditText emailInputLayout;
     TextInputEditText passwordInputLayout;
     ConstraintLayout logInLayout;
+    ImageView logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        emailInputLayout = (TextInputEditText) findViewById(R.id.emailInputField);
         passwordInputLayout = (TextInputEditText) findViewById(R.id.passwordInputField);
         logInLayout = (ConstraintLayout) findViewById(R.id.logInLayout);
+        logInButton = (ImageView) findViewById((R.id.buttonLoggedIn));
+
 
         passwordInputLayout.setOnKeyListener(this);
         logInLayout.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +55,37 @@ public class LogIn extends AppCompatActivity implements View.OnKeyListener {
             }
         });
 
-    }
+        emailInputLayout.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String email = emailInputLayout.getText().toString().trim();
+
+                if (!isValidEmail(email)) {
+                    Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (emailInputLayout.getText().toString().trim() == "" || passwordInputLayout.getText().toString().trim() == "") {
+                    Toast.makeText(getApplicationContext(),"Please enter email and password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    };
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -49,5 +97,15 @@ public class LogIn extends AppCompatActivity implements View.OnKeyListener {
 
     public void goBackToMainScreen(View view) {
         finish();
+    }
+
+    public void goToSignUpPage(View view) {
+        finish();
+        Intent switchActivityIntent = new Intent(getBaseContext(), SignUp.class);
+        startActivity(switchActivityIntent);
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
