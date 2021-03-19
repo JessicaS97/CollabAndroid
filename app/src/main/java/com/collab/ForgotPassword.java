99cb1730cbf3256d561e6dc4df2c70f94789af5d
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-19 23:46:36 +1100

    keyboard hide forgot password

--- app/src/main/java/com/collab/ForgotPassword.java
+++ app/src/main/java/com/collab/ForgotPassword.java
@@ -1,17 +1,39 @@
 package com.collab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    ConstraintLayout forgotPasswordLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        forgotPasswordLayout = (ConstraintLayout) findViewById(R.id.forgotPasswordLayout);
+
+        forgotPasswordLayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                View focusedView = getCurrentFocus();
+
+                if (focusedView != null) {
+                    inputMethodManager.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+                    view.requestFocusFromTouch();
+                }
+            }
+        });
+
     }
 
     public void goBackToMainScreen(View view) {
