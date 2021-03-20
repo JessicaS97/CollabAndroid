199198e43d0025a44559c23ae2ce9306a61aae6d
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-20 19:46:43 +1100

    More page

--- app/src/main/java/com/collab/CreateGroupDetails.java
+++ app/src/main/java/com/collab/CreateGroupDetails.java
@@ -2,13 +2,41 @@ package com.collab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class CreateGroupDetails extends AppCompatActivity {
+public class CreateGroupDetails extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group_details);
+
+        Spinner groupSizeSpinner = findViewById(R.id.dropdownGroupSize);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.group_sizes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groupSizeSpinner.setAdapter(adapter);
+        groupSizeSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String groupSizeSelected = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void goBackToCreateGroupCategoryScreen(View view) {
+        finish();
+        Intent switchActivityIntent = new Intent(getBaseContext(), CreateGroupCategory.class);
+        startActivity(switchActivityIntent);
+        overridePendingTransition(0, 0);
     }
 }
