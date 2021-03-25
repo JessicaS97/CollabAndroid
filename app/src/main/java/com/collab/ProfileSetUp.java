2b4f0540404e9b17c1fd9ab8e1b4a7eb0cc12602
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-25 15:54:35 +1100

    more styling and navigation

--- app/src/main/java/com/collab/ProfileSetUp.java
+++ app/src/main/java/com/collab/ProfileSetUp.java
@@ -2,7 +2,9 @@ package com.collab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileSetUp extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class ProfileSetUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_set_up);
+    }
+
+    public void goBackToExplore(View view) {
+        finish();
+        Intent switchActivityIntent = new Intent(getBaseContext(), ExploreMain.class);
+        startActivity(switchActivityIntent);
+        overridePendingTransition(0, 0);
+    }
+
+    public void finishProfileSetup(View view) {
+        finish();
+        Intent switchActivityIntent = new Intent(getBaseContext(), GroupsMenu.class);
+        startActivity(switchActivityIntent);
+        overridePendingTransition(0, 0);
     }
 }
