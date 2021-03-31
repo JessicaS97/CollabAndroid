19badec0237e4f6699774c8297a7d024058adc78
Jessica J Sun <jessi.slm@hotmail.com>
2021-03-31 16:11:12 +1100

    Adding to welcome page

--- app/src/main/java/com/collab/WelcomePage.java
+++ app/src/main/java/com/collab/WelcomePage.java
@@ -3,6 +3,7 @@ package com.collab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomePage extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+    }
+
+    public void goToProfileSetUpPage(View view) {
+
+    }
+
+    public void goToExplorePage(View view) {
+
     }
 }
