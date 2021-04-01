7ec80547344b506d9161f6716a833fce0e9af4c1
Jessica J Sun <jessi.slm@hotmail.com>
2021-04-01 16:56:04 +1100

    refactoring to make work

---
+++ app/src/main/java/com/collab/AdapterClass.java
@@ -0,0 +1,63 @@
+package com.collab;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nonnull;
+
+public class AdapterClass extends RecyclerView.Adapter<AdapterClass.GroupsViewHolder> {
+    ArrayList<Group> groupList;
+    public AdapterClass(ArrayList<Group> groupList) {
+        this.groupList = groupList;
+    }
+
+    @NonNull
+    @Override
+    public GroupsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.group_individual_layout, parent, false);
+
+        return new GroupsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GroupsViewHolder holder, int position) {
+        holder.groupNameRetrieved.setText(groupList.get(position).getGroupName());
+        holder.groupSizeRetrieved.setText(groupList.get(position).getGroupSize());
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //Glide.with(this).load(user.getPhotoUrl()).into(holder.authorImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return groupList.size();
+    }
+
+    class GroupsViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        TextView groupNameRetrieved, groupSizeRetrieved;
+        ImageView authorImage;
+
+        public GroupsViewHolder(View view) {
+            super(view);
+            groupNameRetrieved = view.findViewById(R.id.groupName);
+            groupSizeRetrieved = view.findViewById(R.id.membersNumber);
+            authorImage = view.findViewById(R.id.profilePic);
+        }
+    }
+}
--- app/src/main/java/com/collab/ExploreMain.java
+++ app/src/main/java/com/collab/ExploreMain.java
@@ -3,11 +3,13 @@ package com.collab;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,11 +42,13 @@ public class ExploreMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_main);
 
-        exploreMenuLayout = (ConstraintLayout) findViewById(R.id.exploreMainLayout);
-        searchFilterExplore = (LinearLayout) findViewById(R.id.searchFilterExplore);
-        editTextTextPersonName2 = (EditText) findViewById(R.id.editTextTextPersonName2);
+        exploreMenuLayout = findViewById(R.id.exploreMainLayout);
+        searchFilterExplore = findViewById(R.id.searchFilterExplore);
+        editTextTextPersonName2 = findViewById(R.id.editTextTextPersonName2);
         databaseReference = FirebaseDatabase.getInstance().getReference("groups");
         groupRecycleView = findViewById(R.id.linearLayout);
+        groupRecycleView.setHasFixedSize(true);
+        groupRecycleView.setLayoutManager(new LinearLayoutManager(this));
 
         exploreMenuLayout.setOnClickListener(new View.OnClickListener() {
 
@@ -64,50 +68,16 @@ public class ExploreMain extends AppCompatActivity {
         });
     }
 
-    public class GroupsViewHolder extends RecyclerView.ViewHolder {
-        View mView;
-        public GroupsViewHolder(View view) {
-            super(view);
-            mView = view;
-        }
 
-        public void setDetails(String groupName, String groupMemberSize) {
-            TextView groupNameRetrieved = mView.findViewById(R.id.groupName);
-            TextView groupSizeRetrieved = mView.findViewById(R.id.membersNumber);
-            ImageView authorImage = mView.findViewById(R.id.profilePic);
-
-            groupNameRetrieved.setText(groupName);
-            groupSizeRetrieved.setText(groupMemberSize);
-
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-            Glide.with(getApplicationContext()).load(user.getPhotoUrl()).into(authorImage);
-        }
-    }
 
     private void firebaseGroupSearch(String searchText) {
         Query firebaseSearchQuery = databaseReference.orderByChild("groupName").startAt(searchText).endAt(searchText + "\uf8ff");
+        Log.i("Search", searchText);
 
         FirebaseRecyclerOptions<Group> options =
                 new FirebaseRecyclerOptions.Builder<Group>()
-                        .setQuery(databaseReference, Group.class)
+                        .setQuery(firebaseSearchQuery, Group.class)
                         .build();
-
-        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Group, GroupsViewHolder>(options) {
-            @Override
-            public GroupsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.activity_explore_main, parent, false);
-
-                return new GroupsViewHolder(view);
-            }
-
-            @Override
-            protected void onBindViewHolder(GroupsViewHolder holder, int position, Group model) {
-                holder.setDetails(model.getGroupName(), model.getGroupSize());
-            }
-        };
-        groupRecycleView.setAdapter(adapter);
     }
 
     public void categorySelected(View view) {
