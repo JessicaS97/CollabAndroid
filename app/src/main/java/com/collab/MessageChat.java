6cddb8aef9b3036086f44f546d4344b2d8fabc23
Jessica J Sun <jessi.slm@hotmail.com>
2021-04-11 15:04:11 +1000

    message chat class

--- app/src/main/java/com/collab/MessageChat.java
+++ app/src/main/java/com/collab/MessageChat.java
@@ -1,8 +1,17 @@
 package com.collab;
 
+import android.os.Bundle;
 import android.view.View;
 
-public class MessageChat {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessageChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message_chat);
+    }
 
     public void goBackToMessagesPage(View view) {
 
