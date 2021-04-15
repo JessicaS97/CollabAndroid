143e8394ca8633a94882b8ccfcb94f1827e3b454
Jessica J Sun <jessi.slm@hotmail.com>
2021-04-15 17:05:18 +1000

    message send class

--- app/src/main/java/com/collab/MessageSend.java
+++ app/src/main/java/com/collab/MessageSend.java
@@ -1,4 +1,27 @@
 package com.collab;
 
-public class MessageSend {
+import java.util.Map;
+
+public class MessageSend extends Message {
+    private Map time;
+
+    public MessageSend() {
+    }
+
+    public MessageSend(Map time) {
+        this.time = time;
+    }
+
+    public MessageSend(String message, String urlPhoto, String name, String profilePic, String message_type, Map time) {
+        super(message, urlPhoto, name, profilePic, message_type);
+        this.time = time;
+    }
+
+    public Map getTime() {
+        return time;
+    }
+
+    public void setTime(Map time) {
+        this.time = time;
+    }
 }
