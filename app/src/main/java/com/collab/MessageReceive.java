2228211acb8f3da3cff1a3903c371a7bcb942dc8
Jessica J Sun <jessi.slm@hotmail.com>
2021-04-15 17:09:18 +1000

    message receive class

--- app/src/main/java/com/collab/MessageReceive.java
+++ app/src/main/java/com/collab/MessageReceive.java
@@ -1,4 +1,25 @@
 package com.collab;
 
-public class MessageReceive {
+public class MessageReceive extends Message {
+    private Long hour;
+
+    public MessageReceive() {
+    }
+
+    public MessageReceive(Long hour) {
+        this.hour = hour;
+    }
+
+    public MessageReceive(String message, String urlPhoto, String name, String profilePic, String message_type, Long hour) {
+        super(message, urlPhoto, name, profilePic, message_type);
+        this.hour = hour;
+    }
+
+    public Long getHour() {
+        return hour;
+    }
+
+    public void setHour(Long hour) {
+        this.hour = hour;
+    }
 }
